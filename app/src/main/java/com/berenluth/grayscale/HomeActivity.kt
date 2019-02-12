876191b01619ccca1f078bb158e820fe8ccda300
Fabio <r.fabio95@gmail.com>
2019-02-12 15:25:53 +0100

    layout refactored

--- app/src/main/java/com/berenluth/grayscale/HomeActivity.kt
+++ app/src/main/java/com/berenluth/grayscale/HomeActivity.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_home.*
 
@@ -59,18 +58,15 @@ class HomeActivity : AppCompatActivity() {
 
 
     fun animateUI(gray: Boolean) {
-        val ANIMATION_SCALE = 175L
+        val animationScale = 175L
         if(gray) {
-            //icon_colors.visibility = View.INVISIBLE
-            //icon_colors.animate().alphaBy(100f).alpha(0f).duration = ANIMATION_SCALE
-            switch_on.animate().scaleX(1f).scaleY(1f).duration = ANIMATION_SCALE
-            switch_off.animate().scaleX(0.7f).scaleY(0.7f).duration = ANIMATION_SCALE
+            switch_on.animate().scaleX(1f).scaleY(1f).duration = animationScale
+            switch_off.animate().scaleX(0.7f).scaleY(0.7f).duration = animationScale
 
         }
         else {
-            //icon_colors.visibility = View.VISIBLE
-            switch_off.animate().scaleX(1f).scaleY(1f).duration = ANIMATION_SCALE
-            switch_on.animate().scaleX(0.7f).scaleY(0.7f).duration = ANIMATION_SCALE
+            switch_off.animate().scaleX(1f).scaleY(1f).duration = animationScale
+            switch_on.animate().scaleX(0.7f).scaleY(0.7f).duration = animationScale
         }
     }
 }
